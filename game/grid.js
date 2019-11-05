c32a26bb5945c1a3ab52421c036aacca25a8aa9b
ammeLacy <emma_lacy@hotmail.com>
2019-11-05 19:26:17 +0000

    added tests and code for process turn and displaying grid on ui

--- game/grid.js
+++ game/grid.js
@@ -2,6 +2,7 @@ const Cell = require('./cell');
 class Grid {
   constructor(cells) {
     this.cells = this.createGrid(cells);
+    this.setUpNeighbours();
   }
 
   createGrid(grid) {
@@ -25,11 +26,68 @@ class Grid {
     return cells;
   }
 
-  processTurn() {
-
+  setUpNeighbours() {
+    for (let i = 0; i < this.cells.length; i++) {
+      for (let j = 0; j < this.cells[i].length; j++) {
+        const cell = this.cells[i][j];
+        //if i -1 or j - 1 are negative neighbour is outside the grid
+        if (i - 1 >= 0 && j - 1 >= 0) {
+          cell.addNeighbour(this.cells[i - 1][j - 1]);
+        }
+        //if i -1 is negative neighbour is outside the grid
+        if (i - 1 >= 0) {
+          cell.addNeighbour(this.cells[i - 1][j]);
+        }
+        //if i -1 is negative or J + 1 is bigger than the grid's width neighbour is outside the grid
+        if (i - 1 >= 0 && j + 1 < this.cells[i].length) {
+          cell.addNeighbour(this.cells[i - 1][j + 1]);
+        }
+        //if j -1 is negative neighbour is outside grid
+        if (j - 1 >= 0) {
+          cell.addNeighbour(this.cells[i][j - 1]);
+        }
+        //if j + 1 is bigger than grid's with neighbour is outside the grid
+        if (j + 1 < this.cells[i].length) {
+          cell.addNeighbour(this.cells[i][j + 1]);
+        }
+        //if i + 1 greater the cells length gone of the bottom of the grid 
+        if (i + 1 < this.cells.length && j - 1 >= 0) {
+          cell.addNeighbour(this.cells[i + 1][j - 1]);
+        }
+        if (i + 1 < this.cells.length) {
+          cell.addNeighbour(this.cells[i + 1][j]);
+        }
+        if (i + 1 < this.cells.length && j + 1 < this.cells[i].length) {
+          cell.addNeighbour(this.cells[i + 1][j + 1]);
+        }
+      }
+    }
   }
 
+  processTurn() {
+    for (let i = 0; i < this.cells.length; i++) {
+      for (let j = 0; j < this.cells[i].length; j++) {
+        this.cells[i][j].assessRules();
+      }
+    }
+    for (let i = 0; i < this.cells.length; i++) {
+      for (let j = 0; j < this.cells[i].length; j++) {
+        this.cells[i][j].applyAssessment();
+      }
+    }
+  }
+
+  //to be used by UI for display purposes
   getCurrentState() {
+    const grid = [];
+    for (let i = 0; i < this.cells.length; i++) {
+      const row = [];
+      for (let j = 0; j < this.cells[i].length; j++) {
+        row.push(this.cells[i][j].isAlive ? 1 : 0);
+      }
+      grid.push(row);
+    }
+    return grid;
   }
 }
 
--- spec/grid.spec.js
+++ spec/grid.spec.js
@@ -16,3 +16,108 @@ describe('createGrid', () => {
     expect(grid.cells[1][0].isAlive).to.equal(false);
   });
 });
+
+describe('setUpNeighbours', () => {
+  it('checks corner top left has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
+    ]);
+    expect(grid.cells[0][0].neighbours.length).to.equal(3);
+  });
+  it('checks corner top right has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]
+    ]);
+    expect(grid.cells[0][2].neighbours.length).to.equal(3);
+  });
+  it('checks corner bottom right has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[2][2].neighbours.length).to.equal(3);
+  });
+  it('checks corner bottom left has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[2][0].neighbours.length).to.equal(3);
+  });
+  it('checks top edge has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[1][0].neighbours.length).to.equal(5);
+  });
+  it('checks left edge has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[0][1].neighbours.length).to.equal(5);
+  });
+  it('checks right edge has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[2][1].neighbours.length).to.equal(5);
+  });
+  it('checks bottom edge has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[1][2].neighbours.length).to.equal(5);
+  });
+  it('checks centre has the right number of neighbour set up', () => {
+    const grid = new Grid([
+      [0, 1, 0],
+      [1, 1, 1],
+      [0, 0, 0]]);
+    expect(grid.cells[1][1].neighbours.length).to.equal(8);
+  });
+});
+
+describe('getCurrentState', () => {
+  it('output matches input', () => {
+    const grid = new Grid(
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ]);
+    const output = grid.getCurrentState();
+    expect(output).to.eql(
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ]);
+  });
+});
+
+describe('processTurn', () => {
+  it('3 cells in a line rotates', () => {
+    const grid = new Grid(
+      [
+        [0, 0, 0],
+        [1, 1, 1],
+        [0, 0, 0]
+      ]);
+    grid.processTurn();
+    const output = grid.getCurrentState();
+    expect(output).to.eql(
+      [
+        [0, 1, 0],
+        [0, 1, 0],
+        [0, 1, 0]
+      ]);
+  });
+});
